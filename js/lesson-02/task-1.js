cca3b2f0c019073f77f63c1d378e322c3f946f8d
Oleg Khodakivskyi <oleg.hodakovsky@gmail.com>
2020-06-23 17:43:13 +0300

    lesson 3 done

--- js/lesson-02/task-1.js
+++ js/lesson-02/task-1.js
@@ -1,10 +1,12 @@
 const logItems = function (array) {
 
-    for (let i = 0; i < array.length; i ++) {
-        let j = array.indexOf(array[i]) + 1 ;
-        console.log(`${j} - ${array[i]}`);
+    for (let i = 0; i < array.length; i +=1 ) {
+        // let j = array.indexOf(array[i]) + 1 ;
+        // console.log(`${j} - ${array[i]}`);
+        console.log(`${i + 1} - ${array[i]}`)
     };
 }
+
 
 logItems(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]);
 logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
---
+++ js/lesson-03/task-6.js
@@ -0,0 +1,29 @@
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
+
+const calculateTotalPrice = function(allProducts, productName) {
+    let totalPrice= 0;
+    for (let i = 0; i < allProducts.length; i +=1) {
+        const entries = Object.values(allProducts[i]);        
+        if (entries.includes(productName)) {
+            totalPrice = entries[1] * entries[2];
+        }
+    }
+    return totalPrice;
+}
+
+
+console.log(calculateTotalPrice(products, 'Радар')); // 5200
+
+console.log(calculateTotalPrice(products, 'Дроид')); // 2800
+
+
+
+// Напиши функцию calculateTotalPrice(allProdcuts, productName), 
+// которая получает массив объектов и имя продукта (значение свойства name).
+// Возвращает общую стоимость продукта (цена * количество).
+// Вызовы функции для проверки работоспособности твоей реализации.
