fecbded565ebe41e9529167dc5427165c24d9dbe
Oleg Khodakivskyi <oleg.hodakovsky@gmail.com>
2020-07-01 14:52:33 +0300

    module 6 fixed

--- js/lesson-06/task-5.js
+++ js/lesson-06/task-5.js
@@ -1,18 +1,26 @@
-    import {default as users} from "./users.js";
+import { default as users } from "./users.js";
 
 const getUserWithEmail = (users, email) => {
-    let result = users.reduce ((acc, user) => {
-        if (user.email === email) {
-            acc.push (user.name)
-        }
-        return acc}, []);
-        return result
+	let result = users.find((user) => user.email === email)
+	return result
 };
 
 
 console.log(getUserWithEmail(users, 'shereeanthony@kog.com'));
- // {объект пользователя Sheree Anthony}
+// {объект пользователя Sheree Anthony}
 console.log(getUserWithEmail(users, 'elmahead@omatom.com'));
  // {объект пользователя Elma Head}
 
 // Получить пользоваля (не массив) по email (поле email, он уникальный).
+
+
+
+// const getUserWithEmail = (users, email) => {
+// 	let result = users.reduce((acc, user) => {
+// 		if (user.email === email) {
+// 			acc.push(user.name) - повертаю саме имена користувачів, а не об'єкти. 
+// 		}
+// 		return acc
+// 	}, []);
+// 	return result
+// };
